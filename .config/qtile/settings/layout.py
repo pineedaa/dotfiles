c409aa902ef631f73ad121161c50b410112c1a1f
Pineedaa <crevi2002@gmail.com>
2022-04-27 16:42:24 +0200

    Codigo refactorizado

--- .config/qtile/settings/layout.py
+++ .config/qtile/settings/layout.py
@@ -3,19 +3,19 @@
 from libqtile import layout
 
 colors = [
-        "#282a36",
-        "#44475a",
-        "#5a5e77",
-        "#717593",
-        "#f8f8f2",
-        "#000000"
+        '#282a36',
+        '#44475a',
+        '#5a5e77',
+        '#717593',
+        '#f8f8f2',
+        '#000000'
         ]
 
 layout_theme = {
-        "margin": 6,
-        "border_width": 3,
-        "border_focus": "#f8f8f2",
-        "border_normal": "#44475a",
+        'margin': 6,
+        'border_width': 3,
+        'border_focus': '#f8f8f2',
+        'border_normal': '#44475a',
     }
 
 layouts = [
--- .config/qtile/settings/mouse.py
+++ .config/qtile/settings/mouse.py
@@ -5,9 +5,9 @@ from libqtile.lazy import lazy
 from .keys import mod
 
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
+    Drag([mod], 'Button1', lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
+    Drag([mod], 'Button3', lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Click([mod], 'Button2', lazy.window.bring_to_front())
 ]
